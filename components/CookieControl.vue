6c1b90dfa496553df312574591713e46e35b0beb
Alexander Trost <galexrt@googlemail.com>
2024-04-23 14:47:30 +0200

    fix: cookie control button ux
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- components/CookieControl.vue
+++ components/CookieControl.vue
@@ -68,7 +68,7 @@ const open = ref(cookiesState.value === null);
                         block
                         color="red"
                         class="flex-1"
-                        :disabled="cookiesState === false"
+                        :variant="cookiesState === false ? 'soft' : 'solid'"
                         @click="
                             cookiesState = false;
                             open = false;
@@ -81,7 +81,7 @@ const open = ref(cookiesState.value === null);
                         block
                         color="green"
                         class="flex-1"
-                        :disabled="cookiesState === true"
+                        :variant="cookiesState === true ? 'soft' : 'solid'"
                         @click="
                             cookiesState = true;
                             open = false;

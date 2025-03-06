21ee2e9de57e98cf4e5e5cf7feeb09de4416f6cc
Alexander Trost <galexrt@googlemail.com>
2025-03-06 10:31:32 +0100

    fix: remove old nuxt config
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,15 +23,6 @@ export default defineNuxtConfig({
 
     compatibilityDate: '2024-07-11',
 
-    hooks: {
-        // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
-        'components:extend': (components) => {
-            const globals = components.filter((c) => ['UButton', 'UIcon'].includes(c.pascalName));
-
-            globals.forEach((c) => (c.global = true));
-        },
-    },
-
     app: {
         baseURL: '/',
         head: {

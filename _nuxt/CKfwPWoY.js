e4c3769c0cd253f5566abc1a88ed35378a353071
galexrt <galexrt@users.noreply.github.com>
2025-04-29 14:48:53 +0000

    deploy: 3b8321ccac3e21e9021f64ee9d7da6ca91a9928b

---
+++ _nuxt/CKfwPWoY.js
@@ -0,0 +1,1 @@
+import{r as t}from"./DK2_EDfq.js";import a from"./BA4TLHkU.js";import{e,f as n,o as s,w as c,u as i}from"./qi3NH3c0.js";import"./BcoM21Dg.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};

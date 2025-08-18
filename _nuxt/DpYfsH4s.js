bd35975bc513370805a42be3d3a97ce2c6d601b9
galexrt <galexrt@users.noreply.github.com>
2025-08-18 14:51:35 +0000

    deploy: 8ce616bb321f76a844061a21f562aefc36b349fa

--- _nuxt/DpYfsH4s.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./CViJ5M0-.js";import t from"./B-e1_uSx.js";import{e as n,f as s,o as a,w as i,u as p}from"./CoE3NRjF.js";import"./iik6CYzq.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(a(),s(t,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[e(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};

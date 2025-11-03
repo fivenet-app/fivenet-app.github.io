9eed621fb7344409a00c39fcb830a45d54f3c577
galexrt <galexrt@users.noreply.github.com>
2025-11-03 14:43:49 +0000

    deploy: c5d8ed8760a248734015a34ade4eabdd4731dc7b

---
+++ _nuxt/Dr3ZD-0z.js
@@ -0,0 +1,1 @@
+import{r as n}from"./C4Nl95ed.js";import a from"./DTtJpBhU.js";import{e,f as s,o as t,w as i,u as p}from"./BpJyVDSs.js";import"./iik6CYzq.js";const d={__name:"ProseWarning",setup(c){const o=e();return(r,f)=>(t(),s(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};

a4b402ef6a2a5dc5d00e2a5d4bfba0e0530b74d5
galexrt <galexrt@users.noreply.github.com>
2026-01-20 14:51:35 +0000

    deploy: ee3d1e269459c5818200a37eca00d61667819bd4

---
+++ _nuxt/CZlIXMO3.js
@@ -0,0 +1,1 @@
+import{e as c,i as n,z as s,c as l,o as p,r as m,A as u}from"./DfZnN83q.js";const i={base:""},_={__name:"ProseEm",props:{class:{type:String,required:!1}},setup(e){const a=e,o=c(),t=n(()=>s({extend:s(i),...o.ui?.prose?.em||{}}));return(r,f)=>(p(),l("em",{class:u(t.value({class:a.class}))},[m(r.$slots,"default")],2))}};export{_ as default};

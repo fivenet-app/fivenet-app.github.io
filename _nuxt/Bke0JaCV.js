2e3119da6ccc27ebf00c8037bb248c4f2fab581f
galexrt <galexrt@users.noreply.github.com>
2026-04-13 22:47:13 +0000

    deploy: 4a3990cc9577f475464b57ce9d0e76c81ca077f7

---
+++ _nuxt/Bke0JaCV.js
@@ -0,0 +1,1 @@
+import{r as s}from"./jXJVAyHf.js";import e from"./DmPOQJL2.js";import{e as t,o as a,f as p,w as c,u as n}from"./C9YjW0s1.js";import"./iik6CYzq.js";const d={__name:"ProseTip",setup(i){const o=t();return(r,f)=>(a(),p(e,{color:"success",icon:n(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
